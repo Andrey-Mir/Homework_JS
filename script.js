9150711834e84d9b7b7793dff5ce48c1022dfed2
Andrey <am8296235@mail.com>
2020-02-29 00:25:31 +0300

    second commit

--- script.js
+++ script.js
@@ -17,30 +17,7 @@ window.onload = function() {
         }
     }
 }
-//button3
-document.getElementById('html').addEventListener('click', function(){
-  updateHTML(
-    `<div style="display: flex;">
-      <div style="width: 25%;">1</div>
-      <div style="width: 25%;">2</div>
-      <div style="width: 25%;">3</div>
-      <div style="width: 25%;">4</div>
-    </div>`
-  );
-});
 
-function changeCSS(){
-  document.head.insertAdjacentHTML('beforeend', `
-     body {
-       background-color: #ffe2aa; color: #123; 
-      } 
-  `);
-}
-
-
-function updateHTML(html){
-  document.body.innerHTML = html;
-}
 
 //Task 2
 var countries = {
